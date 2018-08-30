9b85a16ff88490b88f6644e3245cafcc74d9e4e6
anandshiva10 <anandshiva10@gmail.com>
2018-08-30 08:19:34 +0530

    Add files via upload

---
+++ permissions.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]
+then
+echo "no arguments"
+else
+echo "files containing read,write and execute permissions in the present working directory are:"
+for fname in $(ls)
+do
+if [ -r $fname -a -w $fname -a -x $fname ]
+then
+echo $fname
+fi
+done
